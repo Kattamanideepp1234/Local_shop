97a5a493581a4203a01c801e8a910c6037b0853b
kattamanideepp1234 <sunny19754@gmail.com>
2025-09-21 22:07:32 +0530

    12

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
-});
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api"
+})
 API.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    });
 
 
-export default API;
+    export default API;
